8f74e182bed4b173fe24a0050f2943fc2efb844a
yuri.harutyunyan <yuri.harutyunyan@nork.am>
2025-10-31 18:07:13 +0400

    minor   changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   plugins: [
-    tsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
+    tsConfigPaths(),
     tanstackStart({
-      srcDirectory: 'src',
+      srcDirectory: 'src', // make sure this matches your source folder
     }),
     viteReact(),
   ],
